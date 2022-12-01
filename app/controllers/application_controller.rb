7d85f9e336d9d501a98eb3679af6d66721bfd6c5
angpace <108838753+angpace@users.noreply.github.com>
2022-12-01 10:40:43 -0500

    commit tree c6360a0b75b0727943ae982bc4b39fcbe8a654f4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :do_not_render
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  
+
+    def do_not_render(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def not_found(invalid)
+        render json: { error: "#{invalid.model} not found" }, status: :not_found
+    end
 end
---
+++ app/controllers/scientists_controller.rb
@@ -0,0 +1,33 @@
+class ScientistsController < ApplicationController
+
+    def index
+        render json: Scientist.all
+    end
+
+    def show 
+        scientist = Scientist.find(params[:id])
+        render json: scientist
+    end
+
+    def create
+        scientist = Scientist.create!(scientist_params)
+        render json: scientist, status: :created
+    end
+
+    def update
+        scientist = Scientist.find(params[:id])
+        scientist.update!(scientist_params)
+        render json: scientist, status: :accepted
+    end
+
+    def destroy
+        scientist = Scientist.find(params[:id])
+        scientist.destroy
+    end
+
+    private
+
+    def scientist_params
+        params.permit(:name, :field_of_study, :avatar)
+    end
+end
